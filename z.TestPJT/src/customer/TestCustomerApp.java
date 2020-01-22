69e9f914fd73e535c0acd7ac15d265cf68077e51
user <user@DESKTOP-74IMAPF>
2020-01-22 14:59:28 +0900

    user05: TestCustomerApp.java 		출력문 추가

--- z.TestPJT/src/customer/TestCustomerApp.java
+++ z.TestPJT/src/customer/TestCustomerApp.java
@@ -3,8 +3,8 @@ package customer;
 public class TestCustomerApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+			
+		System.out.println("지지지지지지지지지지지지ㅣ지지지지지지지지지지지지지지지지민이");
 	}
 
 }
